938ed7aea9ff29f83684ffd012015435d756dfe0
nitin pawar <libra11p@gmail.com>
2023-08-25 19:38:46 +0530

    Committing to Feature

---
+++ GitDemoConflits/src/conflictDemo/Fibonacci2.java
@@ -0,0 +1,23 @@
+package conflictDemo;
+
+
+public class Fibonacci2 {
+	public static void main(String[] args) {
+
+		// 0 1 1 2 3 5
+
+		int n1 = 0, n2 = 1, n3 = n1 + n2;
+		int upto = 8;
+		int cnt = 3;
+		System.out.println(n1 + " " + n2 + " " + n3 + " ");
+	
+		while (cnt < upto) {
+			n1 = n2;
+			n2 = n3;
+			n3 = n1 + n2;
+			System.out.println(n3);
+			cnt++;
+		}
+	}
+
+}
